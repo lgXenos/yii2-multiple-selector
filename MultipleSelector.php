eea2ea30cf7e4d01d6e93b02312c5d871c268408
RomanSh <lgcrey@mail.ru>
2019-10-09 21:20:31 +0300

    fix label-panel at new yii-version

--- MultipleSelector.php
+++ MultipleSelector.php
@@ -17,8 +17,9 @@ class MultipleSelector extends InputWidget {
 
 	public function run() {
 		if (!array_key_exists('class', $this->options)) {
-			$this->options['class'] = 'form-control ' . $this->jsSelectClass;
+			$this->options['class'] = 'form-control';
 		}
+		$this->options['class'] .= ' ' . $this->jsSelectClass;
 		$this->options['multiple'] = $this->multiple;
 		$this->options['size']     = $this->size;
 		echo Html::activeDropDownList($this->model, $this->attribute, $this->items, $this->options);
